e01f61bad962823bcc8b7d94d67599a5a7200366
Gustavonn07 <gustavonepnog@gmail.com>
2025-05-21 17:53:14 -0300

    chore: exercise set

---
+++ Random/Studies/set#01.py
@@ -0,0 +1,43 @@
+"""
+    Alunos em atividades extracurriculares
+    Você recebeu os dados de alunos que participam de duas atividades na escola: futebol e xadrez.
+
+    Crie dois conjuntos:
+        Um com os nomes dos alunos que jogam futebol.
+        Outro com os nomes dos alunos que jogam xadrez.
+
+    Usando operações com ‘set’, responda:
+        a) Quem participa das duas atividades?
+        b) Quem participa apenas de futebol?
+        c) Quem participa de pelo menos uma das duas?
+        d) Quem participa somente de uma atividade?
+"""
+
+futebol = {"Ana", "Bruno", "Carlos", "Diana", "Edu"}
+xadrez = {"Carlos", "Fernanda", "Edu", "Gabriel", "Helena"}
+
+from enum import Enum
+
+class SetEnum(Enum):
+    INTERSSECTION = 1
+    UNION = 2
+    DIFFERENCE = 3
+    SYMMETRIC_DIFFERENCE = 4
+
+def get_set_type(enum: SetEnum):
+    types = {
+        SetEnum.INTERSSECTION: '&',
+        SetEnum.UNION: '|',
+        SetEnum.DIFFERENCE: '-',
+        SetEnum.SYMMETRIC_DIFFERENCE: '^',
+    }
+    return types[enum]
+
+def get_group(enum: SetEnum):
+    return eval(f"futebol {get_set_type(enum)} xadrez")
+
+print(f'a) {get_group(SetEnum.INTERSSECTION)} participam das duas atividades')
+print(f'b) {get_group(SetEnum.DIFFERENCE)} participam apenas de futebol')
+print(f'c) {get_group(SetEnum.UNION)} participam de pelo menos uma atividade')
+print(f'd) {get_group(SetEnum.SYMMETRIC_DIFFERENCE)} participam de somente uma atividade')
+
